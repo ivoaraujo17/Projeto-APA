d678561840fa7220cc2f1181214623aff452bc92
HodricVieira <rvo2@academico.ufpb.br>
2024-04-24 21:35:23 -0300

    contando o tempo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <iomanip> // Para manipulação de largura de campo
 #include "read_arquivo.h"
 #include "a_guloso.h"
 #include "VND.h"
@@ -36,30 +38,42 @@ int main(){
     int SVND[6];
     float GAP_guloso[6];
     float GAP_VND[6];
+    float tempo_guloso[6];
+    float tempo_VND[6];
+    chrono::time_point<chrono::system_clock> inicio, fim;
+    chrono::duration<float> duracao;
 
     //dados.print_matriz();
     for (int i = 0; i < 6; ++i){
+
         A_Guloso guloso(arquivos[i]);
+        inicio = chrono::system_clock::now();
         guloso.executar();
+        fim = chrono::system_clock::now();
+        duracao = fim - inicio;
         guloso.solucao.print_solucao();
         print_capacidade_ocupacao(arquivos[i], &guloso.solucao);
         Sguloso[i] = guloso.solucao.custo;
+        tempo_guloso[i] = duracao.count();
         GAP_guloso[i] = ((static_cast<float>(guloso.solucao.custo) - otimos[i]) / otimos[i]) * 100.0f;
 
         VND vnd(&guloso.solucao, arquivos[i], &guloso.jobs_ordenados);
+        inicio = chrono::system_clock::now();
         Solucao nova =  vnd.executar();
+        fim = chrono::system_clock::now();
+        duracao = fim - inicio;
         nova.print_solucao();
         print_capacidade_ocupacao(arquivos[i], &nova);
         SVND[i] = nova.custo;
+        tempo_VND[i] = duracao.count();
         GAP_VND[i] = ((static_cast<float>(nova.custo) - otimos[i]) / otimos[i]) * 100.0f;
-
 
         cout << endl;
     }
 
-    cout << " Instancias " << " Otimo " << " Solucao Guloso " << " Tempo Guloso " << " GAP Guloso " << " Solucao VND " << " Tempo VND " << " GAP VND " << endl;   
+    cout << " Otimo" << setw(10) << " Solucao_Guloso" << setw(10) << " Tempo_Guloso" << setw(10) << " GAP_Guloso" << setw(10) << " Solucao_VND" << setw(10) << " Tempo_VND" << setw(10) <<  " GAP_VND" << endl;
     for (int i = 0; i < 6; ++i){
-        cout << otimos[i]<< " " << Sguloso[i] << " tempo " << GAP_guloso[i] << " " << SVND[i] << " tempo " << GAP_VND[i] << endl; 
+        cout << otimos[i] << setw(12) << Sguloso[i] << setw(12) << tempo_guloso[i] << setw(15) << GAP_guloso[i] << setw(8) << SVND[i] << setw(12) << tempo_VND[i] << setw(12) << GAP_VND[i] << endl; 
     }
 
 
